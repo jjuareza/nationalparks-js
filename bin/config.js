fe40465ed8f794595a1a976de77560f2d4662a42
jjuareza <jorgejuarez_a@hotmail.com>
2018-05-20 16:05:33 -0500

    openstack changes

--- bin/config.js
+++ bin/config.js
@@ -14,7 +14,7 @@ var autoconfig  = function (config_overrides){
 
   var ws_info = {
     id: "nationalparks-js",
-    displayName: "World Parks (JS)",
+    displayName: "World Parks (OpenStack)",
     type: "cluster",
     center: {'latitude': '47.039304', 'longitude': '14.505178'},
     zoom: 4
